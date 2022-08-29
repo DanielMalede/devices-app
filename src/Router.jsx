137c3f47f4e6cc5add7a0fe1621bf2f6d56b089d
DanielMalede <danielmalede@gmail.com>
2022-08-29 23:14:13 +0300

    .

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,35 @@
 import { Routes, Route } from "react-router-dom";
-import { Devices, Home,Movies,MovieDetails } from "./components/index";
+import { Devices, Home, Movies, MovieDetails } from "./components/index";
 import DeviceProvider from "./context/Devices-context/Devices-context";
-import MoviesContext  from "./context/Movie-Context/Movie-Context";
+import MoviesContext from "./context/Movie-Context/Movie-Context";
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="Devices" element={<DeviceProvider><Devices /></DeviceProvider>}/>
-      <Route path="Movies" element={<MoviesContext><Movies /></MoviesContext>}/>
-      <Route path="MovieDetails" element={<MoviesContext><MovieDetails /></MoviesContext>}/>
+      <Route
+        path="Devices"
+        element={
+          <DeviceProvider>
+            <Devices />
+          </DeviceProvider>
+        }
+      />
+      <Route
+        path="Movies"
+        element={
+          <MoviesContext>
+            <Movies />
+          </MoviesContext>
+        }
+      />
+      <Route
+        path="MovieDetails"
+        element={
+          <MoviesContext>
+            <MovieDetails />
+          </MoviesContext>
+        }
+      />
       <Route path="Home" element={<Home />} />
     </Routes>
   );
