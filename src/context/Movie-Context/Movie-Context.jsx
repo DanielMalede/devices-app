0f6d48d81721ea32bc7ec010ef12e13d11d42d3c
DanielMalede <danielmalede@gmail.com>
2022-08-30 15:24:46 +0300

    .

--- src/context/Movie-Context/Movie-Context.jsx
+++ src/context/Movie-Context/Movie-Context.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 import "./Movie-Context.css";
 
 export const moviesContext = createContext();
-function MoviesProvider({ children }) {
+function MoviesProvider({children}) {
   const [movies, setMovies] = useState([]);
   return (
     <moviesContext.Provider value={{ movies, setMovies }}>
