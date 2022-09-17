d7e2696fc227b87cc418acf47f12d003100ad6be
DanielMalede <danielmalede@gmail.com>
2022-09-17 23:34:57 +0300

    .

--- src/service/movies.service.js
+++ src/service/movies.service.js
@@ -3,7 +3,6 @@ export default async function moviesApi() {
         return await fetch('https://my-json-server.typicode.com/Jeck99/movies-api/movies')
             .then(res => res.json())
     } catch (error) {
-
     }
-    
+
 }
