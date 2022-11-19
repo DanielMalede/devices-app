0575d8cee073a20479c93fd3b361017afa58ac13
DanielMalede <danielmalede@gmail.com>
2022-11-19 19:40:44 +0200

    .

--- src/service/devices.service.js
+++ src/service/devices.service.js
@@ -1,8 +1,7 @@
 export async function devicesApi() {
   try {
-    return await fetch('https://my-json-server.typicode.com/Jeck99/fake-server/devices')
-      .then(res => res.json())
-  } catch (error) {
-
-  }
+    return await fetch(
+      "https://my-json-server.typicode.com/Jeck99/fake-server/devices"
+    ).then((res) => res.json());
+  } catch (error) {}
 }
